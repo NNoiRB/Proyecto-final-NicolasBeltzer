1770faef66d1e25698f28e2ebd0ee7446848324f
NNoiRB <132013917+NNoiRB@users.noreply.github.com>
2023-05-15 22:04:00 -0300

    proceso final :D
    tp

--- proyecto-final-nicolasbeltzer/src/app/app.module.ts
+++ proyecto-final-nicolasbeltzer/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
@@ -19,4 +21,5 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
